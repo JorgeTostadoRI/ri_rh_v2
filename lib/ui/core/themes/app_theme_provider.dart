1be530803c9447e8f6d36790fd237d70c87f0dc0
Jorge Tostado <sistemas@riautomation.solutions>
2026-06-24 09:55:05 -0700

    added input theme to theme provider

--- lib/ui/core/themes/app_theme_provider.dart
+++ lib/ui/core/themes/app_theme_provider.dart
@@ -8,16 +8,40 @@ const Color labelTextColor = Color(0xFF9A7B5A);
 const Color successColor = Color(0xFF52C41A);
 const Color errorColor = Color(0xFFDB4444);
 const Color backgroundColor = Color(0xFFFFF0DC);
+const Color borderColor = Color(0xFFF5E8D8);
+const Color inputFillColor = Color(0xFFFDFBF7);
 
 class AppThemeProvider extends ChangeNotifier {
   bool darkMode = false;
 
   ThemeData get appTheme => ThemeData(
-        useMaterial3: true,
-        colorSchemeSeed: primaryColor,
-        brightness: darkMode ? Brightness.dark : Brightness.light,
-        fontFamily: 'Inter',
-      );
+    useMaterial3: true,
+    colorSchemeSeed: primaryColor,
+    brightness: darkMode ? Brightness.dark : Brightness.light,
+    fontFamily: 'Inter',
+    inputDecorationTheme: InputDecorationTheme(
+      labelStyle: TextStyle(
+        color: Color(0xFFC4A47A),
+        fontWeight: .w700,
+      ),
+      border: WidgetStateInputBorder.fromMap({
+        WidgetState.focused: OutlineInputBorder(
+          borderSide: BorderSide(color: primaryColor, width: 0.8),
+          borderRadius: BorderRadius.circular(16),
+        ),
+        WidgetState.error: OutlineInputBorder(
+          borderSide: BorderSide(color: errorColor, width: 0.8),
+          borderRadius: BorderRadius.circular(16),
+        ),
+        WidgetState.any: OutlineInputBorder(
+          borderSide: BorderSide(color: borderColor, width: 0.8),
+          borderRadius: BorderRadius.circular(16),
+        ),
+      }),
+      filled: true,
+      fillColor: inputFillColor,
+    ),
+  );
 
   void toggleBrightness(bool value) {
     darkMode = value;
