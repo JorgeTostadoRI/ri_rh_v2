3cc36b173d9db365a10459d4b56fa9628834f779
Jorge Tostado <sistemas@riautomation.solutions>
2026-06-26 15:04:14 -0700

    added incidencias local repository

--- lib/data/repositories/incidencias/incidencias_repository.dart
+++ lib/data/repositories/incidencias/incidencias_repository.dart
@@ -1,1 +1,7 @@
-abstract class IncidenciasRepository {}
+import 'package:ri_rh_v2/domain/models/incidencias/incidencia.dart';
+import 'package:ri_rh_v2/utils/result.dart';
+
+abstract class IncidenciasRepository {
+  /// Crea una solicitud de [Incidencia]
+  Future<Result<void>> createIncidencia(Incidencia incidencia);
+}
