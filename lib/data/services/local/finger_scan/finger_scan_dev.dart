0afc009fe416076a858be26aeadc3fe48a33aa02
Jorge Tostado <sistemas@riautomation.solutions>
2026-07-09 14:18:05 -0700

    fix no FingerScanService for local providers

---
+++ lib/data/services/local/finger_scan/finger_scan_dev.dart
@@ -0,0 +1,13 @@
+import 'package:ri_rh_v2/data/services/local/finger_scan/finger_scan_service.dart';
+
+class FingerScanDev extends FingerScanService {
+  @override
+  void init() {
+    // TODO: implement init
+  }
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+  }
+}
