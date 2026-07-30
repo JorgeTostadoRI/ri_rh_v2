80eaa373d15f27ec2109a9209bde5802afd8e2d7
Jorge Tostado <sistemas@riautomation.solutions>
2026-07-30 09:44:49 -0700

    Add isSameDay extension for DateTime

--- lib/utils/datetime_extensions.dart
+++ lib/utils/datetime_extensions.dart
@@ -1,7 +1,13 @@
-extension DateTimeStringExtensions on DateTime {
+extension ExtendedDateTime on DateTime {
+  /// Returns the date in YYYY-MM-DD format.
   String toShortIsoString() {
     final paddedMonth = month.toString().padLeft(2, '0');
     final paddedDay = day.toString().padLeft(2, '0');
     return '$year-$paddedMonth-$paddedDay';
+  }
+
+  /// Returns whether this date is in the same day of the year as [other].
+  bool isSameDay(DateTime other) {
+    return year == other.year && month == other.month && day == other.day;
   }
 }
