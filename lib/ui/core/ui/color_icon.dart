23e5e08c0a10db69c96918b390d1ce8f22f25c98
Jorge Tostado <sistemas@riautomation.solutions>
2026-07-30 09:45:59 -0700

    Add ColorIcon widget

---
+++ lib/ui/core/ui/color_icon.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:ri_rh_v2/ui/core/themes/app_theme_provider.dart';
+
+class ColorIcon extends StatelessWidget {
+  final Color? backgroundColor;
+  final Color? iconColor;
+  final double width;
+  final double height;
+  final IconData icon;
+
+  const ColorIcon({
+    super.key,
+    this.backgroundColor,
+    this.iconColor,
+    this.width = 48.0,
+    this.height = 48.0,
+    required this.icon,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: width,
+      height: height,
+      decoration: BoxDecoration(
+        color: backgroundColor,
+        borderRadius: BorderRadius.circular(15),
+      ),
+      child: Icon(icon, color: iconColor ?? primaryColor),
+    );
+  }
+}
